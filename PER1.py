fda0a274a33af4e8b5df5a78d9af412ee2a220d7
Laura <lauritaadiaz21@gmail.com>
2019-01-21 16:22:34 +0100

    Cambioo

--- PER1.py
+++ PER1.py
@@ -1,5 +1,6 @@
 def Duck(grupo,numero):
     print("La oca es:", grupo[numero])
+    print("Hola")
     return
 grupo = ["Laura", "Ruben", "Carla", "Paula", "Pepe", "Carlos", "Francis"]
 Duck(grupo,numero = int(input("Introduce un numero del 0 al 6: ")))
